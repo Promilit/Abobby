7c185d41141592f34ee99c54d0e63e771a892d7f
Promilit <mike@Mikes-MacBook-Air.local>
2023-07-06 11:37:54 -0400

    first commit

---
+++ config.py
@@ -0,0 +1,13 @@
+OPENAI_API_KEY = "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"
+ELEVEN_LABS_API_KEY = "3f1a01966f809236079c459b6de3f492"
+
+ADVISOR_IMAGE_WIDTH = 360
+ADVISOR_IMAGE_HEIGHT = 360
+
+# ADVISOR_IMAGE = "images/samantha.png"
+# ADVISOR_VOICE_ID = "z0Joqjbi82gHWKvf8hw3"
+# ADVISOR_CUSTOM_PROMPT = "Answer in the style of Precious, a friendly female assistant."
+
+ADVISOR_IMAGE = "images/Mikidigi_smiling_black_man_8d5de74e-50b6-4e0b-9294-f64a2813938e.png"
+ADVISOR_VOICE_ID = "Cw9jG6FPMzt26j2qCQgZ"
+ADVISOR_CUSTOM_PROMPT = ""
